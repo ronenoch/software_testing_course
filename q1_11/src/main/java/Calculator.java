fd554ff85684772f577e7539637f78b878423354
Ron Enoch <ronenoch13@gmail.com>
2023-04-04 01:28:34 +0300

    added some tests.
    Signed-off-by: Ron Enoch <ronenoch13@gmail.com>

--- q1_11/src/main/java/Calculator.java
+++ q1_11/src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.stream.DoubleStream;
 
 public class Calculator {
@@ -10,5 +12,25 @@ public class Calculator {
     static double multiply(double... operands) {
         return DoubleStream.of(operands)
                 .reduce(1, (a, b) -> a * b);
+    }
+    static Integer returnNULL() {
+        return null;
+    }
+    static Object raise_exception() throws Exception {
+        throw new Exception();
+    }
+
+    static double divide(double... operands) {
+//        for (double i : Arrays.stream(operands).toArray()) {
+        for (double i : Arrays.stream(operands).skip(1).toArray()) {
+            if (0 == i) {
+                throw new ArithmeticException();
+            }
+        }
+        return DoubleStream.of(operands)
+                .reduce((a, b) -> a / b).getAsDouble();
+    }
+    static double divide_q2(double... operands) {
+        return divide(operands);
     }
 }
--- q1_11/src/test/java/CalculatorTest.java
+++ q1_11/src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,17 +6,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-//    @DisplayName("Add two numbers")
+    @DisplayName("Add two numbers")
     void add() {
         assertEquals(4, Calculator.add(2, 2));
     }
 
     @Test
-//    @DisplayName("Multiply two numbers")
+    @DisplayName("Multiply two numbers")
     void multiply() {
         assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
                 () -> assertEquals(-4, Calculator.multiply(2, -2)),
                 () -> assertEquals(4, Calculator.multiply(-2, -2)),
                 () -> assertEquals(0, Calculator.multiply(1, 0)));
     }
+
+    @Test
+    void divide() {
+
+        assertEquals(1, Calculator.divide(2, 2), 1.0E-15);
+        assertEquals(0, Calculator.divide(0.000001, 1000000000), 1.0E-15);
+        assertNotEquals(4, Calculator.divide(2.00000000000001, 2), 1.0E-15);
+    }
+    @Test
+    void divide_zero() {
+
+        assertThrows(ArithmeticException.class, () -> {
+            Calculator.divide(2, 0);
+        });
+    }
+
+    @Test
+    void raise_exception() {
+
+        assertThrows(Exception.class, () -> {
+            Calculator.raise_exception();
+        });
+
+    }
+    @Test
+    void returnNull() {
+
+        Calculator calc = new Calculator();
+        calc.returnNULL();
+    }
+
+    @Test
+    void divide_q2() {
+        Calculator calc = new Calculator();
+        calc.divide(1,1);
+
+    }
+
+
 }
